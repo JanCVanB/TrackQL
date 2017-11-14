32efe556b1541b70dd9e6461fdf0d48a1bd81edd
Jan Van Bruggen <jancvanbruggen@gmail.com>
2017-11-14 15:25:50 -0800

    :hammer: Translate App.vue from HTML/CSS to Pug/Sass

--- vue-client/src/App.vue
+++ vue-client/src/App.vue
@@ -1,23 +1,20 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+<template lang="pug">
+  #app
+    router-view
 </template>
 
 <script>
 export default {
+
   name: 'app'
+
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+#app
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  margin: 60px
 </style>
